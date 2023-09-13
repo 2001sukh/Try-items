2e8cfe1236d4a739be3240464aca18a4180ce010
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-09-12 18:21:22 -0700

    Variables are added
    item, stock, wanted, deliver , can_buy

--- index.php
+++ index.php
@@ -75,7 +75,14 @@ $wanted  = 5;
 $can_buy = ($wanted <= $stock);
 ?>
 
-
+<!-- // logical-operators-->
+<?php 
+$item    = 'Chocolate';
+$stock   = 5;
+$wanted  = 3;
+$deliver = true;
+$can_buy = (($wanted <= $stock) && ($deliver == true));
+?>
 
 
 <!DOCTYPE html>
@@ -177,6 +184,18 @@ $can_buy = ($wanted <= $stock);
     <p>Item:    <?= $item ?></p>
     <p>Stock:   <?= $stock ?></p>
     <p>Wanted:  <?= $wanted ?></p>
+    <p>Can buy: <?= $can_buy ?></p>
+
+    <!-- // logical-operators-->
+    <title>Logical Operators</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <h1>The Candy Store</h1>
+    <h1>Shopping Cart</h1>
+    <p>Item:    <?= $item ?></p>
+    <p>Stock:   <?= $stock ?></p>
+    <p>Wanted: <?= $wanted ?></p>
     <p>Can buy: <?= $can_buy ?></p>
 
   </body>
