2abc6289c17422caf6b46395948e4a413f6c65bd
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-09-12 17:39:40 -0700

    updated  arrays nutrition values

--- index.php
+++ index.php
@@ -64,6 +64,13 @@ $nutrition['protein'] = 7.3;
 
 
 <!-- updating arrays nutrition values  --> 
+<h1>The Candy Store</h1>
+    <h2>Nutrition (per 100g)</h2>
+    <p>Fat:   <?php echo $nutrition['fat']; ?>%</p>
+    <p>Sugar: <?php echo $nutrition['sugar']; ?>%</p>
+    <p>Salt:  <?php echo $nutrition['salt']; ?>%</p>
+    <p>Fiber: <?php echo $nutrition['fiber']; ?>%</p>
+  </body>
 
 
   </body>
