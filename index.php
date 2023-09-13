6b8a21557588a733158c6f753ca12e42ba3f690f
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-09-12 17:51:00 -0700

    Added name of the product linked with index number
    for example number =0
    product= toffee

--- index.php
+++ index.php
@@ -40,7 +40,8 @@ $offers = [
     ['name' => 'Mints',  'price' => 3, 'stock' => 66,],
     ['name' => 'Fudge',  'price' => 4, 'stock' => 97,],
     ['name' => 'chocholate',  'price' => 2, 'stock' => 83,],
-
+  ];
+  ?>
 
 <!DOCTYPE html>
 <html>
@@ -79,7 +80,21 @@ $offers = [
     <p>Sugar: <?php echo $nutrition['sugar']; ?>%</p>
     <p>Salt:  <?php echo $nutrition['salt']; ?>%</p>
     <p>Fiber: <?php echo $nutrition['fiber']; ?>%</p>
-  </body>
+  
+
+<!-- // Multidimensional arrays  -->
+    <title>Multidimensional Arrays</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <h1>The Candy Store</h1>
+    <h2>Offers</h2>
+    <p><?php echo $offers[0]['name']; ?> -
+      $<?php echo $offers[0]['price']; ?> </p>
+    <p><?php echo $offers[1]['name']; ?> -
+      $<?php echo $offers[1]['price']; ?> </p>
+    <p><?php echo $offers[2]['name']; ?> -
+      $<?php echo $offers[2]['price']; ?> </p>
 
 
   </body>
