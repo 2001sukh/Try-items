1a9b7da434b94cdd4092f51424c3b95e87d8d70a
2001sukh <113390606+2001sukh@users.noreply.github.com>
2023-09-12 17:10:29 -0700

    Added new nutrition products
    fat
    sugar
    salt

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php 
+// variables
 $name  = 'Sukhdeep';
+
+// updating variables
 $name  = 'Harry';
 $price = 2;
+
+// updating arrays
+$nutrition = [
+  'fat'  => 16,
+  'sugar' => 51,
+  'salt' => 6.3,
+];
 ?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -14,5 +25,14 @@ $price = 2;
     <h2>Welcome <?php echo $name; ?></h2>
     <p>The cost of your candy is 
        $<?php echo $price; ?> per pack.</p>
+
+<!-- updating array -->
+       <h2> $Nutrition (per 100g) </h2>
+       <p>Fat:   <?php  echo $nutrition['fat']; ?>%</p>
+       <p>Sugar:   <?php  echo $nutrition['sugar']; ?>%</p>
+       <p>Salt:   <?php  echo $nutrition['salt']; ?>%</p>
+
+
+
   </body>
 </html>
